2223db7e169568c508edb0b801dfa9f8a81c0192
Guillermo Leon <guilevasa@gmail.com>
2024-06-20 17:36:51 -0500

    Se crea la carpeta debug con el archivo index.js para revisar la opcion de debugger, el cual permite revisar el codigo hasta donde se emplee esta palabra reservada en javascript, en la consola de chrome

---
+++ src/debug/index.js
@@ -0,0 +1,12 @@
+var a = "Hello";
+
+function hello() {
+    let b = "Hello World";
+    const c = "Hello World!"
+    debugger;
+}
+
+hello();
+
+/* la palabra reservada "debugger" permite correr el codigo hasta donde se encuentre esta palabra y ralizarle una revision,
+esto por medio del navegador chrome, en la consola*/
